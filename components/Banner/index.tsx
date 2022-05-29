5aacc7535e8fe94b21605e261daa58cbfd8cd978
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-05-29 12:23:43 -0300

    Terminado a area do banner

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,12 +1,19 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './styles.module.css';
 import 'swiper/css';
+import { Autoplay } from 'swiper';
 
 export const Banner = () => {
     return   (      
         <div className={styles.container}>
         <Swiper
             slidesPerView={1}
+            loop={true}
+            autoplay={{
+                delay: 1000,
+                disableOnInteraction: false
+              }}
+              modules={[Autoplay]}
             className={styles.swiper}
             >
             <SwiperSlide className={styles.slide}><img src="/tmp/banner1.png" alt=''/></SwiperSlide>
